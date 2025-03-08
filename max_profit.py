811e3dbd2d385375bc0fe6deb83f4deaf24fb7c4
Wilbert Tham <94099663+phoonicked@users.noreply.github.com>
2025-03-08 12:14:21 +0000

    Practice 1: Maximum Profit in a Single Transaction

---
+++ max_profit.py
@@ -0,0 +1,18 @@
+from typing import List
+
+def max_profit(prices: List[int]) -> int:
+    if not prices:
+        return 0
+    buy_price = prices[0]
+    max_profit_so_far = 0
+    for price in prices:
+        if price < buy_price:
+            buy_price = price
+        else:
+            profit = price - buy_price
+            if profit > max_profit_so_far:
+                max_profit_so_far = profit
+    return max_profit_so_far
+
+print(max_profit([100, 180, 260, 310, 40, 535, 695]))
+        
